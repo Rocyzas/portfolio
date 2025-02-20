473f80f4ebe2de71803fd99faeb2311580d9194a
Rocyzas <pranas.rokas@gmail.com>
2025-02-20 19:46:41 +0000

    Add projects and resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,7 @@ const about = {
   role: 'Software Engineer',
   description:
     'Katarsis laimes eurovizija.',
-  resume: 'https://example.com',
+  resume: 'https://drive.google.com/file/d/1EG5uUkooWlBh6BC3hoYPRcbGRO1d_qV_/view?usp=drive_link',
   social: {
     linkedin: 'https://www.linkedin.com/in/rokas-pranevicius/',
     github: 'https://github.com/Rocyzas/',
@@ -22,11 +22,23 @@ const contact = {
 
 const projects = [
     {
-        name: "asdasdXX",
-        description: "adsasdasd'",
-        stack: ['React', 'JavaScript', 'CSS'],
-        sourceCode: "asdasd",
-        livePreview: "asdasd",
+        name: "Image Classification App",
+        description: "Muffin and Chihuahua image classification app deployed on Azure.",
+        stack: ['Python', 'Tensorflow', 'Flask'],
+        sourceCode: "https://github.com/Rocyzas/Muffin-vs-Chihuahua-Classification",
+        // livePreview: "asdasd",
+      },
+      {
+        name: "Lindenmayer Systems",
+        description: "Performed genetic operations on L-Systems using crossover and mutation, visualising patterns of growing biological structures.",
+        stack: ['Python', 'Turtle'],
+        sourceCode: "https://github.com/Rocyzas/Genetic-operations-on-L-systems",
+      },
+      {
+        name: "Alzheimer's Disease diagnosis",
+        description: "Classified MRI hippocampus scans between healthy cases, mildy impared and Alzheimer's achieving 84% accuracy",
+        stack: ['Python', 'Tensorflow', 'Keras', 'Hippodeep'],
+        sourceCode: "https://github.com/Rocyzas/Explainable-AI-Classifiers-for-Alzheimers-Disease-Diagnosis",
       }
 ];
 
