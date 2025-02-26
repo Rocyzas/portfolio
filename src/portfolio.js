9d6dafe4cf081f9a6937b34acc819b6d570c981a
Rocyzas <pranas.rokas@gmail.com>
2025-02-26 21:23:04 +0000

    add more projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -39,6 +39,19 @@ const projects = [
         description: "Classified MRI hippocampus scans between healthy cases, mildy impared and Alzheimer's achieving 84% accuracy",
         stack: ['Python', 'Tensorflow', 'Keras', 'Hippodeep'],
         sourceCode: "https://github.com/Rocyzas/Explainable-AI-Classifiers-for-Alzheimers-Disease-Diagnosis",
+      },
+      {
+        name: "OpenGL Cybertruck",
+        description: "Designed and implemented rotating Cybertruck in OpenGL with UI, Hierarchical Modelling, Textures, Normals",
+        stack: ['C++', 'OpenGL'],
+        sourceCode: "https://github.com/Rocyzas/OpenGL-cybertruck",
+      },
+      {
+        name: "Fake News Detection App",
+        description: "Implemented Multimodal fake news detection classifier to identify suspicious post from Reddit",
+        stack: ['Python', 'Pytorch', 'Transformers'],
+        sourceCode: "https://gitlab.com/ACopley/fakenews_detector/-/tree/classifier",
+        livePreview: 'https://drive.google.com/file/d/1axT6V4lmZXPWTOobiH4_Yc503uTepuXO/view?usp=sharing'
       }
 ];
 
